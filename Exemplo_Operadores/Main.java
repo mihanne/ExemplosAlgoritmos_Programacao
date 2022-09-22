cd62dcd5b8ac475b3f5cc8201ca8526948e72f98
Michelle Hanne Soares de Andrade <michelle.hanne@ormuztech.com.br>
2022-09-22 18:53:19 +0000

    exemplos_alterados

---
+++ Exemplo_Operadores/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    double resultado;
+    double resto;
+    resultado= 10/3;
+    resto = 10%3;
+    System.out.println("Resultado da Divisão de 10 por 3: " + resultado);
+    System.out.println("Resto da Divisão de 10 por 3: " + resto);
+    }
+}
